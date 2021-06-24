5b597380368673e5093a828aaa23ea9fd7315133
Alejandro Laurora <alelaurora@gmail.com>
2021-06-24 00:30:47 -0300

    Pre-Production Version
    Pre-Production Version

--- script.js
+++ script.js
@@ -3,3 +3,75 @@ $(document).ready(function() {
     var rand = Math.floor(Math.random() * colors.length);
     $('.change').css("background-color", colors[rand]);
 });
+
+// $('a').click(function(){
+//     $('html, body').animate({
+//         scrollTop: $( $(this).attr('href') ).offset().top
+//     }, 500);
+//     return false;
+// });
+
+var words = ['Making digital things easier so you can live your life.','Smile througt digital.','I work so you can have a delightful experiences.', 'bridge'],
+    currentStep = 0,
+    textEl = document.querySelector('.change-text'),
+    oldWord = '';
+
+
+setTimeout(changeWord, 800);
+
+function changeWord() {
+    oldWord = textEl.innerHTML;
+
+    // check if there is a word atm or not
+    if (oldWord.length < 1) {
+
+        if (currentStep !== words.length -1) {
+            currentStep ++;
+        }else {
+        currentStep = 0;
+        }
+        addNextWord();
+    } else {
+        setTimeout(deleteWord, 400);
+    }
+};
+
+function deleteWord() {
+  var WordLength = oldWord.length,
+      currentWord = textEl.innerHTML,
+      currentLength = currentWord.length;
+  
+
+  // The word is deleted so, start adding in the new one
+  if (currentLength < 1) {
+    changeWord();
+    return;
+  }
+  
+  // Remove a charachter
+  textEl.innerHTML = currentWord.substring(0, currentLength - 1);
+  
+  setTimeout(deleteWord, 70);
+}
+
+function addNextWord() {
+  var currentWord = textEl.innerHTML,
+      currentLength = currentWord.length,
+      nextWord = words[currentStep],
+      nextWordLength = nextWord.length;
+    
+  
+  if (currentLength === nextWordLength) {
+    changeWord();
+    return;
+  }
+  
+  // add a charachter
+  textEl.innerHTML = nextWord.substring(0, currentLength + 1);
+    
+  setTimeout(addNextWord, 200);
+  
+  
+}
+
+
