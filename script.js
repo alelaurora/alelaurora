242a3c0f384af8e7ba96ade05a6457f158448168
Alejandro Laurora <alelaurora@gmail.com>
2022-04-11 17:20:35 -0300

    Portfolio page
    First portfolio test, commenting junk, adding animation css

--- script.js
+++ script.js
@@ -70,8 +70,24 @@ function addNextWord() {
   textEl.innerHTML = nextWord.substring(0, currentLength + 1);
     
   setTimeout(addNextWord, 200);
-  
-  
 }
 
+//Get the button
+var mybutton = document.getElementById("btn-go-top");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
